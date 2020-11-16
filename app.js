c65dc415bd3ece84e46ce1f5fc1e9c73b3dc5d30
deepdas <deepdas2105@gmail.com>
2020-11-16 01:09:34 -0800

    add success and failiur pages & signup and login page complete

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const alert = require('js-alert')
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+
+let userDetails = [""];
+
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.render('index');
@@ -33,3 +39,25 @@ app.get("/products",function(req,res){
 app.listen(3000,function(){
     console.log("server started at port 3000");
 })
+
+app.post("/signup",function(req,res){
+    let count = 0;
+    const userSignup={
+         name: req.body.userName,
+         email: req.body.userEmail,
+         password: req.body.password,
+         contact: req.body.contact,
+         address: req.body.address
+    }
+        for (let i = 1; i < userDetails.length; i++) {
+          if (userDetails[i].email === userSignup.email) {   
+                count++;
+          }
+         }
+         if (count == 0) {
+            userDetails.push(userSignup);      
+            res.render("success");
+         } else {
+            res.render('failiur');
+         }
+    });

24e4fd0cc96641bddc7790eca910ac4fcb856e73
Deep <deepdas2105@gmail.com>
2021-02-05 21:10:53 +0530

    complete project

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-const { fileLoader } = require('ejs');
-const { static } = require('express');
+const ejs = require('ejs');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));//static files
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/productsDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 const productSchema = new mongoose.Schema({
     name: {
@@ -22,21 +42,27 @@ const productSchema = new mongoose.Schema({
 
 const userSchema = new mongoose.Schema({
     name: String,
-    email: String,
-    password: String,
+    username: String,
     contact: Number,
     address: String,
     productsBrought: Array
+})
+
+const customerSchema = new mongoose.Schema({
+    name: String,
+    products: Array
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("user",userSchema);
 const Product = mongoose.model("product",productSchema);
+const Customer = mongoose.model("customer",customerSchema);
 
+passport.use(User.createStrategy());
 
-app.set('view engine', 'ejs');
-
-app.use(express.static("public"));//static files
-app.use(bodyParser.urlencoded({extended:true}));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render('index');
@@ -50,123 +76,150 @@ app.get("/login",function(req,res){
     res.render('login');
 })
 
-app.get("/settings",function(req,res){
-
-    res.render("settings");
-})
-
 app.get("/cart",function(req,res){
-        res.render('cart',{products: productArray});
+    if(req.isAuthenticated()){
+        const productsArray = req.user.productsBrought;
+        res.render('cart',{products: productsArray});
+    } else {
+        res.redirect('/login');
+    }
 })
 
-
-app.listen(3000,function(){
-    console.log("server started at port 3000");
+app.get("/products",function(req,res){
+    if(req.isAuthenticated()){
+        res.render('products');
+    } else {
+        res.redirect('/login');
+    }
 })
+
+app.get('/logout',function(req,res){
+    req.logout();
+    res.redirect('/');
+})
+
 
 //signup
 app.post("/signup",function(req,res){
-    User.findOne({email: req.body.userEmail,password: req.body.password},function(err,founditem){
-        if(founditem === null)
+    
+    User.register({
+        name: req.body.Name,
+        username: req.body.username,
+        contact: req.body.contact,
+        address: req.body.address
+    },req.body.password,function(err,user){
+        if(err)
         {
-            const user = new User({
-                name: req.body.userName,
-                email: req.body.userEmail,
-                password: req.body.password,
-                contact: req.body.contact,
-                address: req.body.address
-           });
-            user.save(function(err){
-                if(!err)
-                {
-                    res.redirect("/login");
-                }
-            })
+            console.log(err);
+            res.redirect('/');
         } else {
-          res.render("failiur",{failiurMassage: "It seems You are already a member of our family.Try to login instead."});
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/products");
+            })
         }
-    })   
+    })
+
     });
 
 //login page
 
 app.post("/login",function(req,res) {
-    User.findOne({email: req.body.email,password: req.body.password},function(err,founditem){
-        if(founditem !== null)
-        {
-            res.redirect('/'+founditem._id);
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user,function(err){
+        if(err){
+            console.log(err);
         } else {
-            res.render("failiur",{failiurMassage: "Your Account doesn't exist in our database.Try to Signup Instead!"});
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/products");
+            })
         }
     })
 })
 
-app.get("/:name",function(req,res){
-    const id = req.params.name;
-    res.render('products',{
-        id: id
-    });
-    
-})
-
-let productArray = [];
 app.post("/products",function(req,res){
-    const title = req.body.title;
-    const price = req.body.price;
-    const id = req.body.id;
-    const product = new Product({
-        name: title,
-        price: price
-    });
-    productArray.push(product);
-     User.updateOne({_id: id},{productsBrought: productArray},function(err){
-        if(!err){
-            console.log("successfully updated");
-        }
-    });
-    Product.findOne({name: title,price: price},function(err,founditem){
-        if(founditem === null)
+    Product.countDocuments({name: req.body.title , price: req.body.price},function(err,count){
+        if(!err)
         {
-            product.save(function(err){
-                if(!err)
-                {
-                    res.redirect("/"+id);
-                }
-            });
-        } else {
-            res.redirect("/"+id);
+            if(count === 0)
+            {
+                const product = new Product({
+                name: req.body.title,
+                price: req.body.price
+                });
+                product.save();
+                var userArray = req.user.productsBrought;
+                userArray.push(product);
+                User.findById(req.user._id,function(err,foundUser){
+                    if(!err)
+                    {
+                        foundUser.productsBrought = userArray;
+                        foundUser.save(function(err){
+                            if(!err)
+                            {
+                                res.redirect("/products");
+                            }
+                        });
+                    }
+                }) 
+            }
         }
     });
+    Product.findOne({name: req.body.title,price: req.body.price},function(err,foundProduct){
+        if(!err)
+        {
+            var userArray = req.user.productsBrought;
+                userArray.push(foundProduct);
+                User.findById(req.user._id,function(err,foundUser){
+                    if(!err)
+                    {
+                        foundUser.productsBrought = userArray;
+                        foundUser.save(function(error){
+                            if(!error){
+                                res.redirect("/products");
+                            }
+                        });
+                    }
+                })
+        }
+    })
 })
 
 
 app.post("/cart",function(req,res){
+    User.findById(req.user._id,function(err,foundUser){
+        if(!err)
+        {
+            const newCustomer = new Customer({
+                name: foundUser._id,
+                products: foundUser.productsBrought
+            })
+            newCustomer.save();
+            foundUser.productsBrought = [];
+            foundUser.save();
+        }
+    })
     res.render("success",{successMassage: "Your Order has been Placed.Thank you for shopping with us"});
 })
 
-app.post("/settings",function(req,res){
-    const email = req.body.email;
-    const oldPassword = req.body.oldPassword;
-    const newPassword = req.body.newPassword;
-    User.updateOne({email: email,password:oldPassword},{password:newPassword},function(err){
-        if(err){
-            res.render("failiur",{failiurMassage: "There is a problem while setting up your password.Please try again later."});
-        } else {
-            res.render("success",{successMassage: 'Successfully Updated your new Password.Please Login with  your new Password'});
+app.post("/delete",function(req,res){
+    User.findById(req.user._id,function(err,foundUser){
+        if(!err)
+        {
+            const productsBrought = req.user.productsBrought;
+            productsBrought.splice(req.body.item,1);
+            foundUser.productsBrought = productsBrought;
+            foundUser.save(function(error){
+                if(!error){
+                    res.redirect("/cart");
+                }
+            });
         }
     })
 })
 
-app.post("/delete",function(req,res){
-    const deleteItem = req.body.item;
-
-    User.find({productsBrought:productArray},function(err,founditem){
-        const id = founditem._id;
-        productArray.splice(deleteItem,1);
-        User.updateOne({id: id},{productsBrought: productArray},function(err){
-        if("!err")
-        res.redirect("/cart");
-    })
-    })
-    
+app.listen(3000,function(){
+    console.log("server started at port 3000");
 })
